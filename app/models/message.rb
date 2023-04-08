d99d5a1fd91052239447c6bf003489995bbf5173
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-04-08 02:04:42 +0000

    messages work, woohoo

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,4 +15,13 @@ class Message < ApplicationRecord
   belongs_to :sender, { :required => true, :class_name => "User", :foreign_key => "sender_id" }
 
   belongs_to :recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id" }
+
+
+  def thread_key
+    if sender_id < recipient_id
+      "#{sender_id}-#{recipient_id}"
+    else
+      "#{recipient_id}-#{sender_id}"
+    end
+  end
 end
