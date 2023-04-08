94da51a2d2f108e11bdb9b92c94717e25c3c9bb3
Paulina Delmore <105574611+pdelmore@users.noreply.github.com>
2023-04-08 16:00:39 +0000

    clean up messages page, correct time zone

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -27,14 +27,15 @@ module FinalProject
       g.test_framework nil
       g.factory_bot false
       g.scaffold_stylesheet false
-      g.stylesheets     false
-      g.javascripts     false
-      g.helper          false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
     end
-    
+
     config.action_controller.default_protect_from_forgery = false
     config.active_record.belongs_to_required_by_default = false
 
+    config.time_zone = "Central Time (US & Canada)"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -48,7 +49,7 @@ module FinalProject
       ActionDispatch::Static,
       Rails.root.join("docs").to_s,
       index: config.public_file_server.index_name,
-      headers: config.public_file_server.headers || {}
+      headers: config.public_file_server.headers || {},
     )
   end
 end
